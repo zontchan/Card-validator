a38c17e6718416f347e4c302ae0e8123bfdbaa61
zontchan <semenovajuls@icloud.com>
2023-07-31 21:19:08 +0700

    added e2e test

---
+++ src/js/__tests__/checkValidity-e2e.test.js
@@ -0,0 +1,36 @@
+import puppetteer from 'puppeteer';
+jest.setTimeout(30000); // default puppeteer timeout
+describe('check validity of card number', () => {
+    let browser = null;
+    let page = null;
+    const baseUrl = 'http://localhost:9000';
+    beforeAll(async () => {
+        browser = await puppetteer.launch({
+            //headless: false, // show gui
+            //slowMo: 100
+            //devtools: true, // show devTools
+        });
+        page = await browser.newPage();
+    });
+    afterAll(async () => {
+        await browser.close();
+    });
+        test('should add .valid class for valid card number', async () => {
+            await page.goto(baseUrl);
+            const form = await page.$('.form');
+            const input = await form.$('card-input');
+            await page.$eval('.card-input', el => el.value = '4929620273994371');
+            const submit = await form.$('.button');
+            submit.click();
+            await page.waitForSelector('.result.valid');
+        });
+    test('should add .invalid class for invalid card number', async () => {
+        await page.goto(baseUrl);
+        const form = await page.$('.form');
+        const input = await form.$('card-input');
+        await page.$eval('.card-input', el => el.value = '4929620273994372');
+        const submit = await form.$('.button');
+        submit.click();
+        await page.waitForSelector('.result.invalid');
+    });
+});
